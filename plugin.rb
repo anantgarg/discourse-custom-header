498680a54be0af8ec2ef3fb0f7af3d0d48afeba3
Anant Garg <anant.garg@inscripts.com>
2019-08-22 12:10:04 -0600

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,9 +10,9 @@ after_initialize do
 
  # load File.expand_path('../app/views/layouts/_head.html.erb', __FILE__)
 
-#  Rails.configuration.paths["app/views"].unshift(File.expand_path("../app/views", __FILE__))
+ Rails.configuration.paths["app/views"].unshift(File.expand_path("../app/views", __FILE__))
        
-# remove_file 'app/views/layouts/_head.html.erb'
- gsub_file  'app/views/layouts/_head.html.erb',/.*/,'TEST'
+# remove_file 'app/views/layouts/_head.html.erb'#
+# gsub_file  'app/views/layouts/_head.html.erb',/.*/,'TEST'
 
 end
